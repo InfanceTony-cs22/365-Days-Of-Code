4a33d1098011cf3a3e00270e26dfc4c720e3eb81
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2024-01-03 18:31:15 +0530

    Create Day 2 .py

---
+++ Day 2 .py
@@ -0,0 +1,18 @@
+def main():
+    # Assign values to the following variable as described above
+    # Don't change the variable name
+    my_string1 = "InterviewBit"
+    my_string2 = "Don't change the variable name"
+    my_int = 11
+    my_float = 20.0
+    
+    # Don't change the below code
+    print("String1: %s" % my_string1)
+    print("String2: %s" % my_string2)
+    print("Integer: %d" % my_int)
+    print("Float: %f" % my_float)
+    return 0
+
+if __name__ == '__main__':
+    main()
+
