796b82392f535a51bc49ef242b79cfa3fc5b443a
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2024-02-02 18:18:44 +0530

    Create Day 32.py

---
+++ Day 32.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param A : string
+    # @return an integer
+    def titleToNumber(self, A):
+        result = 0
+
+        for char in A:
+            result = result * 26 + (ord(char) - ord('A') + 1)
+
+        return result
