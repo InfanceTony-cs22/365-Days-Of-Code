bd4988e5c94ba010a94eab4ba4601976c4d197e1
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2024-03-25 15:59:11 +0530

    Create Day 84.py

---
+++ Day 84.py
@@ -0,0 +1,23 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Assign values to the following variables as described above
+        Don't change the variable names
+        */
+        String mystring1 = "InterviewBit";
+        String mystring2 = "Don't change the variable name";
+        int myint = 11;
+        boolean myboolean = true;
+        
+        
+        /*Don't change the code below*/
+        System.out.println("String1 = " + mystring1);
+        System.out.println("String2 = " + mystring2);
+        System.out.println("int = " + myint);
+        System.out.println("boolean = " + myboolean);
+        
+    }
+}
