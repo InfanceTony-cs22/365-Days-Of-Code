33896d1ab05acb4f31bc14aedc53b3710ec21384
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2024-03-26 19:16:48 +0530

    Create Day 85.py

---
+++ Day 85.py
@@ -0,0 +1,24 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        /***Don't change the code here***/
+        
+        Scanner inp  = new Scanner(System.in);
+        int M = inp.nextInt();
+        inp.close();
+        /*********************************/
+        
+        /**Write your code here**/
+        if (M % 3 == 0 && M % 5 == 0) {
+            System.out.println("Good Number");
+        } else if (M % 3 == 0 && M % 5 != 0) {
+            System.out.println("Bad Number");
+        } else if (M % 3 != 0 && M % 5 == 0) {
+            System.out.println("Poor Number");
+        } else {
+            System.out.println("-1");
+        }
+    }
+}
