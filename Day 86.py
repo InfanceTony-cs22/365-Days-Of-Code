baf04cd27f4ec449c060b81a1757f31b3cf3e57d
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2024-03-27 18:40:44 +0530

    Create Day 86.py

---
+++ Day 86.py
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        /***Don't change the code here***/
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+        inp.close();
+        /***********************************/
+        
+        
+        /***Your code goes here***/
+        for (int i = 1; i <= n; ++i) {
+            System.out.println(i);
+        }
+    }
+}
+
