db1982a959e59b611adab20d1a69c6ac9c0a9a73
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2024-04-22 18:28:02 +0530

    Create Day 112.py

---
+++ Day 112.py
@@ -0,0 +1,21 @@
+#include<iostream>
+#include <iomanip> 
+using namespace std;
+
+int main()  {
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+
+    cin >> i >> l >> c >> f >> d;
+
+    cout << i << endl;
+    cout << l << endl;
+    cout << c << endl;
+    cout << fixed << setprecision(3) << f << endl;
+    cout << fixed << setprecision(9) << d << endl;
+
+    return 0;
+}
