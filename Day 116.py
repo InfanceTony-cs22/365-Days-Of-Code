d68d00936f8488a23254981fe17e5736a2e22402
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2024-04-26 19:18:52 +0530

    Create Day 116.py

---
+++ Day 116.py
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    
+    for (int i = 0; i < N; ++i) {
+        if (i % 2 != 0) {
+            cout << i << endl;
+        }
+    }
+    
+    return 0;
+}
